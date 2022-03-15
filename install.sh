ae9257339bbd0da40609f51fb9ce742083d2f102
salty <salty@salty.dk>
2022-03-15 17:59:24 +0100

    add the correct docker apt repository when not using AMD64 arch.

--- install.sh
+++ install.sh
@@ -50,7 +50,7 @@ function dockerUbuntu() {
         echo -e "\e[32mInstalling Docker for Ubuntu"
         echo -e "\e[39mPlease be patient"
         curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-        add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+        add-apt-repository "deb [arch=$(dpkg --print-architecture)] signed-by=/usr/share/keyrings/docker-archive-keyring.gpg https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
         apt install docker-ce -y
         echo -e "\e[39mDocker Installed"
         usermod -aG docker "$install_user"
